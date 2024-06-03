a264e9baf74533edd1acc318ea6d4b7d89e25431
Davuluri Sai Sujan <138643678+saisujan42@users.noreply.github.com>
2024-06-03 11:30:23 +0530

    Add files via upload

---
+++ 1. Learn The Basics/1.2. Build-Up Logical Thinking/14. Pattern (C++).cpp
@@ -0,0 +1,9 @@
+void nLetterTriangle(int n) 
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(char ch = 'A'; ch < 'A' + i; ch++)
+            cout << ch << " ";
+        cout << "\n";
+    }
+}
---
+++ 1. Learn The Basics/1.2. Build-Up Logical Thinking/6. Pattern (C++).cpp
@@ -0,0 +1,9 @@
+void nNumberTriangle(int n) 
+{
+    for(int i = n; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+            cout << j << " ";
+        cout << "\n";
+    }
+}
