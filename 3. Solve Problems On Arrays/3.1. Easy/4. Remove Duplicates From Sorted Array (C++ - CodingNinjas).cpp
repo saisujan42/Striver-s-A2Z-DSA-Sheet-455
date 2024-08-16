0218ea4a309268d915654a1a130ded8d64647c65
Davuluri Sai Sujan <138643678+saisujan42@users.noreply.github.com>
2024-08-16 21:03:32 +0530

    Delete 3. Solve Problems On Arrays directory

--- 3. Solve Problems On Arrays/3.1. Easy/4. Remove Duplicates From Sorted Array (C++ - CodingNinjas).cpp
+++
@@ -1,8 +0,0 @@
-int removeDuplicates(vector<int> &arr, int n) 
-{
-	int Count = 0;
-	for(int i = 0; i < n - 1; i++)
-		if(arr[i] == arr[i + 1])
-			Count++;
-	return n - Count;
-}
--- 3. Solve Problems On Arrays/3.1. Easy/8. Linear Search (C++).cpp
+++
@@ -1,7 +0,0 @@
-int linearSearch(int n, int key, vector<int> &arr)
-{
-    for(int i = 0; i < n; i++)
-        if(arr[i] == key)
-            return i;
-    return -1;
-}
