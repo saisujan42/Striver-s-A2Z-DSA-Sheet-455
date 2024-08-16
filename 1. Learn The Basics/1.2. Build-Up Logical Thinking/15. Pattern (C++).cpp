c18904260600b5b65390c7c67ac48adbe38860d4
Davuluri Sai Sujan <138643678+saisujan42@users.noreply.github.com>
2024-08-16 20:57:04 +0530

    Delete 1. Learn The Basics/1.2. Build-Up Logical Thinking directory

--- 1. Learn The Basics/1.2. Build-Up Logical Thinking/15. Pattern (C++).cpp
+++
@@ -1,9 +0,0 @@
-void nLetterTriangle(int n) 
-{
-    for(int i = n; i >= 1; i--)
-    {
-        for(char ch = 'A'; ch < 'A' + i; ch++)
-            cout << ch << " ";
-        cout << "\n";
-    }
-}
